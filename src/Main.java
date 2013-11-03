45fac634bdedff2d59bb11cf4b470517ecae6ca3
Moncef <moncef@metanote.local>
2013-11-03 14:19:46 +0100

    new test project

---
+++ src/Main.java
@@ -0,0 +1,12 @@
+/**
+ * Created with IntelliJ IDEA.
+ * User: moncef
+ * Date: 03/11/13
+ * Time: 14:07
+ * To change this template use File | Settings | File Templates.
+ */
+public class Main {
+    public static void main (String[] args){
+        System.out.println("Hello World");
+    }
+}
